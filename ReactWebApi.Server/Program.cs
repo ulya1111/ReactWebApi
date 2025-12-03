92d483c6f7c04aef298686fb860416b9bbd8a753
max1n <ilybro961@gmail.com>
2025-12-03 20:54:35 +0300

    Добавлены изначальные данные в бд

--- ReactWebApi.Server/Program.cs
+++ ReactWebApi.Server/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.EntityFrameworkCore;
 using ReactWebApi.Server.Data;
+using ReactWebApi.Server.Models;
 using System;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -37,8 +38,33 @@ app.MapFallbackToFile("index.html");
 using (var scope = app.Services.CreateScope())
 {
     var db = scope.ServiceProvider.GetRequiredService<AppDbContext>();
+
     db.Database.Migrate();
+
+    if (!db.Items.Any())
+    {
+        db.Items.AddRange(
+            new Item
+            {
+                Name = "Clean Code",
+                Value = "Robert C. Martin (Best practices for writing clean, maintainable code)"
+            },
+            new Item
+            {
+                Name = "Design Patterns",
+                Value = "Erich Gamma et al. (Classic book about software architecture patterns)"
+            },
+            new Item
+            {
+                Name = "The Pragmatic Programmer",
+                Value = "Andrew Hunt & David Thomas"
+            }
+        );
+
+        db.SaveChanges();
+    }
 }
+
 
 app.Run();
 
